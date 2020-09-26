306b787b0fe3f4d11af2c33416d2bdce73067f9b
Arkadii Ivanov <arkann1985@gmail.com>
2020-09-26 20:00:03 +0100

    Update documentation

--- keepers/src/androidMain/kotlin/com/arkivanov/mvikotlin/keepers/instancekeeper/AndroidInstanceKeeper.kt
+++ keepers/src/androidMain/kotlin/com/arkivanov/mvikotlin/keepers/instancekeeper/AndroidInstanceKeeper.kt
@@ -8,3 +8,19 @@ fun <T : ViewModelStoreOwner> T.getInstanceKeeper(): InstanceKeeper =
     ViewModelProvider(this)
         .get(InstanceKeeperViewModel::class.java)
         .instanceKeeper
+
+class CalculatorController(instanceKeeper: InstanceKeeper) {
+
+    private val something: Something =
+        instanceKeeper.getOrCreate(::Something)
+
+    /*
+     * Instances of this class will be retained.
+     * ⚠️ Pay attention to not leak any dependencies.
+     */
+    private class Something : InstanceKeeper.Instance {
+        override fun onDestroy() {
+            // Clean-up any resources here
+        }
+    }
+}
